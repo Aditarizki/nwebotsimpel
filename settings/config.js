e783a54f79dd8c8cdf7ba5ac8dd0e91b5a26f737
Aditarizki <101645278+Aditarizki@users.noreply.github.com>
2022-03-16 18:12:39 +0700

    config.js

--- settings/config.js
+++ settings/config.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="YOOBOTZ OFC"
-global.ownernumber = "6285290270235"
-global.botname = "YOOBOTZ"
+global.ownername ="ADITA OFC"
+global.ownernumber = "62895386009665"
+global.botname = "ADTZBOTZ"
 global.thumbnail = fs.readFileSync("./settings/haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.limit = {
